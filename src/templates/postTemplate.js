fe862ca6f6ec291e027fd7cdbdd2dfdd0424c786
Luis Ramos <luis@luisramos.app>
2020-11-24 11:21:37 +1100

    Add tweet/email callout to posts layout

--- src/templates/postTemplate.js
+++ src/templates/postTemplate.js
@@ -22,10 +22,37 @@ export default function PostTemplate({ location, data }) {
         html={html}
         slug={frontmatter.slug}
       />
-      {/* Improve this area with comments/ callouts for people to reach out!*/}
+      <FriendlyCallout />
     </Layout>
   )
 }
+
+const FriendlyCallout = () => (
+  <div class="flex-col items-center pt-6">
+    <div class="mx-auto w-12 border-b-2 border-orange-300 text-center mb-4"></div>
+    <div class="flex justify-start">
+      <div class="flex-col px-4 py-4">
+        <h4 class="text-base mb-2">Thank you for reading</h4>
+        <p class="text-sm opacity-75 mb-4">
+          Please get in touch if you have a suggestion, spot an issue, or want
+          to say hello.
+        </p>
+        <div class="flex-row">
+          <a href="mailto:luis.ramos@hey.com">
+            <button class="text-sm rounded text-black border-2 border-gray-300 py-1 px-2">
+              Send email
+            </button>
+          </a>
+          <a href="https://twitter.com/luisramos1337">
+            <button class="text-sm rounded border-2 border-blue-200 text-blue-600 py-1 px-2 ml-4">
+              Send tweet
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+)
 
 export const pageQuery = graphql`
   query($path: String!) {
