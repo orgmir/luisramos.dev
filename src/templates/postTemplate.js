093ae2e734e726473c1f85bd64b72ebb4d54303f
Luis Ramos <luis@luisramos.app>
2021-04-12 09:20:53 +1000

    Add Edit in Github button to all posts

--- src/templates/postTemplate.js
+++ src/templates/postTemplate.js
@@ -3,53 +3,55 @@ import { graphql } from 'gatsby'
 import Post from '../components/post'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-// import { Disqus } from 'gatsby-plugin-disqus'
 
 export default function PostTemplate({ location, data }) {
-  const { markdownRemark /*, site */ } = data
+  const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
-  // const disqusConfig = {
-  //   url: `${site.siteMetadata.siteUrl + location.pathname}`,
-  //   identifier: frontmatter.slug,
-  //   title: frontmatter.title,
-  // }
   return (
     <Layout location={location}>
       <SEO title={frontmatter.title} slug={frontmatter.slug} />
-      {frontmatter.draft === true ? (
+      {frontmatter.draft && (
         <div class="blockquote alert">
           <i>You are looking at a draft post.</i>
         </div>
-      ) : null}
+      )}
       <Post
         title={frontmatter.title}
         date={frontmatter.date}
         html={html}
         slug={frontmatter.slug}
       />
-      <FriendlyCallout />
+      <FriendlyCallout postRelativePath={data.file.relativePath}/>
     </Layout>
   )
 }
 
-const FriendlyCallout = () => (
+const FriendlyCallout = ({postRelativePath}) => (
   <div class="flex-col items-center pt-6">
-    <div class="mx-auto w-12 border-b-2 border-orange-300 text-center mb-4"></div>
+    <div className="mx-auto w-12 border-b-2 border-orange-300 text-center mb-4" />
     <div class="flex justify-start">
       <div class="flex-col px-4 py-4">
         <h4 class="text-base mb-2">Thank you for reading</h4>
         <p class="text-sm opacity-75 mb-4">
-          Please get in touch if you have a suggestion, spot an issue, or want
+          Please get in touch if you have a suggestion, spot an issue or want
           to say hello.
         </p>
         <div class="flex-row">
+          { postRelativePath != null && (
+            <a href={"https://github.com/Orgmir/luisramos.dev/blob/main/src/content/"+postRelativePath}>
+              <button class="text-sm rounded text-black border-2 border-gray-300 py-1 px-2 mr-4">
+                Edit in Github
+              </button>
+            </a>
+            )
+          }
           <a href="mailto:luis.ramos@hey.com">
-            <button class="text-sm rounded text-black border-2 border-gray-300 py-1 px-2">
+            <button class="text-sm rounded text-black border-2 border-gray-300 py-1 px-2 mr-4">
               Send email
             </button>
           </a>
           <a href="https://twitter.com/luisramos1337">
-            <button class="text-sm rounded border-2 border-blue-200 text-blue-600 py-1 px-2 ml-4">
+            <button class="text-sm rounded border-2 border-blue-200 text-blue-600 py-1 px-2">
               Send tweet
             </button>
           </a>
@@ -74,6 +76,9 @@ export const pageQuery = graphql`
       siteMetadata {
         siteUrl
       }
+    }
+    file {
+      relativePath
     }
   }
 `
