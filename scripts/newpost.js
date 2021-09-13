9b2e32fa7985ccef8c8aeaa0d016880ff0c61f76
Luis Ramos <luis@luisramos.app>
2021-09-13 11:51:28 +1000

    New post

---
+++ scripts/newpost.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const slugify = require('slug')
+const dateFns = require('date-fns')
+
+const title = process.argv[2]
+if (!title) {
+	throw 'a title is required!'
+}
+const slug = slugify(title.toLowerCase())
+const date = dateFns.format(new Date(), 'yyyy-MM-dd')
+const dir = `./src/content/${date}-${slug}`
+if (!fs.existsSync(dir)) {
+	fs.mkdirSync(dir)
+} else {
+	throw 'That post already exists!'
+}
+fs.writeFileSync(
+	`${dir}/index.md`,
+	`---
+date: ${date}
+title: "${title}"
+slug: /${slug}
+---`,
+	function (err) {
+		if (err) {
+			return console.log(err)
+		}
+		console.log(`${title} was created!`)
+	}
+)
--- src/templates/postTemplate.js
+++ src/templates/postTemplate.js
@@ -21,38 +21,42 @@ export default function PostTemplate({ location, data }) {
         html={html}
         slug={frontmatter.slug}
       />
-      <FriendlyCallout postRelativePath={data.file.relativePath}/>
+      <FriendlyCallout postRelativePath={data.file.relativePath} />
     </Layout>
   )
 }
 
-const FriendlyCallout = ({postRelativePath}) => (
+const FriendlyCallout = ({ postRelativePath }) => (
   <div class="flex-col items-center pt-6">
     <div className="mx-auto w-12 border-b-2 border-orange-300 text-center mb-4" />
     <div class="flex justify-start">
       <div class="flex-col px-4 py-4">
-        <h4 class="text-base mb-2">Thank you for reading</h4>
+        <h4 className="text-base mb-2">Thank you for reading</h4>
         <p class="text-sm opacity-75 mb-4">
-          Please get in touch if you have a suggestion, spot an issue or want
-          to say hello.
+          Please get in touch if you have a suggestion, spot an issue or want to
+          say hello.
         </p>
         <div class="flex-row">
-          { postRelativePath != null && (
-            <a href={"https://github.com/Orgmir/luisramos.dev/blob/main/src/content/"+postRelativePath}>
+          {postRelativePath != null && (
+            <a
+              href={
+                'https://github.com/Orgmir/luisramos.dev/blob/main/src/content/' +
+                postRelativePath
+              }
+            >
               <button class="text-sm rounded text-black border-2 border-gray-300 py-1 px-2 mr-4">
                 Edit in Github
               </button>
             </a>
-            )
-          }
+          )}
           <a href="mailto:luis.ramos@hey.com">
             <button class="text-sm rounded text-black border-2 border-gray-300 py-1 px-2 mr-4">
-              Send email
+              📧 Email me
             </button>
           </a>
           <a href="https://twitter.com/luisramos1337">
             <button class="text-sm rounded border-2 border-blue-200 text-blue-600 py-1 px-2">
-              Send tweet
+              🐥 Tweet at me!
             </button>
           </a>
         </div>
