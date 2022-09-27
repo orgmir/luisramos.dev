cf932ef831962a1164c2349fbfc9eccfc7958e83
Luis Ramos <luis.ramos@hey.com>
2022-09-27 10:07:14 +0100

    Add width and height to image

--- src/components/hello.js
+++ src/components/hello.js
@@ -5,7 +5,7 @@ import eu from '../images/EU.png'
 const Hello = () => (
   <div className="flex text-gray-900 justify-center items-center text-center">
     <div className="">
-      <img className="rounded-full" src={eu} alt="Luis' Face" />
+      <img className="rounded-full" src={eu} alt="Luis' Face" width="90" height="90"/>
     </div>
     <h1 className="flex-initial text-3xl sm:text-5xl font-bold ml-2 sm:ml-6">
       Olá! I'm Luis
